d73561361f57e57073e54cc599fadfa0865ffc54
Meera Benson <meeratucson@yahoo.com>
2016-02-16 22:18:09 -0700

    Initial version

---
+++ Calculator/src/cse360assign3/Calculator.java
@@ -0,0 +1,88 @@
+package cse360assign3;
+
+/** 
+ * Computes arithmetic operations on a total value
+ * 
+ *  @author Meera Benson PIN:121
+ *  @version Feb 22, 2016
+ *  
+ */
+
+public class Calculator 
+{
+
+	private int total;
+	
+	/**
+	 * Initiates the total to 0
+	 * 
+	 * @param none
+	 */
+	public Calculator () 
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Returns the total
+	 * 
+	 * @param none
+	 * @return 0        Zero for now
+	 */
+	public int getTotal () 
+	{
+		return 0;
+	}
+	
+	/**
+	 * Adds a value to the total
+	 * 
+	 * @param value      The value to be added
+	 */
+	public void add (int value) 
+	{
+		
+	}
+	
+	/**
+	 * Subtracts a value from the total
+	 * 
+	 * @param value      The value to be subtracted
+	 */
+	public void subtract (int value) 
+	{
+		
+	}
+	
+	/**
+	 * Multiplies the total by a value
+	 * 
+	 * @param value      The value to multiply by
+	 */
+	public void multiply (int value) 
+	{
+		
+	}
+	
+	/**
+	 * Divides the total by a value
+	 * 
+	 * @param value      The value to divide by
+	 */
+	public void divide (int value) 
+	{
+		
+	}
+	
+	/**
+	 * Returns the operations performed in a string
+	 * 
+	 * @param none
+	 * @returns ""       An empty string for now
+	 */
+	public String getHistory () 
+	{
+		return "";
+	}
+	
+}
